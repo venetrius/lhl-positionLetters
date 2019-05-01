f83c17323c423955c3dc43a63b8f083e0678d5cf
venetrius <gergelygjuhasz@gmail.com>
2019-05-01 13:02:13 -0600

    codereviewed with mentor

--- letter-positions.js
+++ letter-positions.js
@@ -2,17 +2,31 @@ function positionLetters(string){
   var positions = {};
   for (let i = 0; i < string.length; i++){
     let nextChar = string[i];
-    if(positions[nextChar] === undefined){
+    if(nextChar === " "){
+      continue;
+    }
+    if(!positions[nextChar]){
       positions[nextChar] = [i];
     }else{
       positions[nextChar].push(i);
     }
-  }
-  if(positions[" "]){
-    delete positions[" "];
   }
   return positions;
 }
 
 var string = process.argv[2];
 console.log(positionLetters(string));
+
+/*
+expected output with "lighthouse in the house":
+{ l: [ 0 ],
+  i: [ 1, 11 ],
+  g: [ 2 ],
+  h: [ 3, 5, 15, 18 ],
+  t: [ 4, 14 ],
+  o: [ 6, 19 ],
+  u: [ 7, 20 ],
+  s: [ 8, 21 ],
+  e: [ 9, 16, 22 ],
+  n: [ 12 ] }
+*/
